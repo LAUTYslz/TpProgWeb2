369cb5fbccd74d7301f5b9fcb0bc64196f8adf60
AgustinAnichini <agustinanichini277@gmail.com>
2024-04-27 12:19:58 -0300

    correccion login

--- components/header.php
+++ components/header.php
@@ -3,7 +3,7 @@
         <img src="img/logo.svg" alt="Logo">
     </div>
     <h1>Pokedex</h1>
-    <form action="../validaciones/login.php" method="post">
+    <form action="validaciones/login.php" method="post">
         <input type="text" placeholder="usuario" name="usuario">
         <input type="text" placeholder="contraseña" name="pass">
         <input type="submit" value="Ingresar">
